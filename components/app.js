a17856b951bf2e28a31b15db2fd026293e5a140d
Adam Nelip <Adam.Nelip@gmail.com>
2018-07-09 13:14:34 +0200

    Working app, editing styles.css

---
+++ components/app.js
@@ -0,0 +1,40 @@
+var contacts = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
+];
+
+var contactForm = {
+  firstName: '',
+  lastName: '',
+  email: ''
+};
+
+var App = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {className: 'app'},
+        React.createElement(ContactForm, {contact: contactForm}),
+        React.createElement(Contacts, {items: contacts}, {})
+      )
+    );
+  }
+});
